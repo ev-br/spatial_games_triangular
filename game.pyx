395a40d3c4e5f339e8dd8bb0e28d5574c05593d1
AlezM <aamalyutin@phygitalism.com>
2018-04-07 14:13:36 +0300

    Remove initialization and show method from GameField class

--- game.pyx
+++ game.pyx
@@ -10,8 +10,8 @@ cdef extern from "evolve.cc":
 
 cdef class GameField:
     cdef vector[int] _field
-    cdef int L
-    cdef double b
+    cdef int _L
+    cdef double _b
 
     def __init__(self, L, b, per, seed=0):
         pass
@@ -24,32 +24,23 @@ cdef class GameField:
 #        for i in range(L*L):
 #            self._field[i] = 0
 
-    def __cinit__(self, int L, double b, double per, int seed=0):
-        self.L = L
-        self.b = b
+    def __cinit__(self, int L, double b):
+        self._L = L
+        self._b = b
         self._field.resize(L*L)
-
-        if seed == -1:
-            self._field = np.array([int(i == L*L // 2) for i in range(L*L)])
-        else:
-            if not seed == 0:
-                np.random.seed(seed)
-                
-            for i in range(L*L):
-                self._field[i] = int(100 * np.random.rand() < per)
 
     @property
     def field(self):
         """Return the field as a numpy array."""
         cdef npy_intp dims[2]
-        dims[0] = self.L 
-        dims[1] = self.L
+        dims[0] = self._L 
+        dims[1] = self._L
         return PyArray_SimpleNewFromData(2, dims, NPY_INT, &self._field[0])
 
     @property
-    def size(self):
+    def L(self):
         """Return field size."""
-        return self.L
+        return self._L
 
     @field.setter
     def field(self, arr):
@@ -57,8 +48,8 @@ cdef class GameField:
         arr = np.asarray(arr)
         if len(arr.shape) != 2:
             raise ValueError("Expected a 2D array, got %s-d." % len(arr.shape))
-        if arr.size != self.L*self.L:
-            raise ValueError("Size mismatch: expected %s, got %s." % (self.L*self.L, arr.size))
+        if arr.size != self._L*self._L:
+            raise ValueError("Size mismatch: expected %s, got %s." % (self._L*self._L, arr.size))
 
         arr = arr.ravel()
         for j in range(arr.size):
@@ -67,14 +58,6 @@ cdef class GameField:
     def evolve(self, int num_steps=1):
         with nogil:
             evolve_field(self._field, self.b, num_steps)
-
-    def show(self, point_size=10, scale=1):
-        plt.clf()
-        plt.figure(figsize = (scale*10, scale*6.66))
-        y, x = (1-self.field).nonzero()
-        plt.scatter(x + y*np.sin(np.pi/6), y * np.sin(np.pi/3), s=point_size, marker='h')
-        y, x = self.field.nonzero()
-        plt.scatter(x + y*np.sin(np.pi/6), y * np.sin(np.pi/3), s=point_size, marker='h', c='r')
 
 #### init the numpy C API
 import_array()
